228991b281bdf078652052853871676f4d14144f
FarhanMasood <farhanmasoodkhan2@gmail.com>
2022-07-23 16:33:10 +0500

    Commit #02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textarea from './components/Textarea';
 
 function App() {
   return (
     <>
       <Navbar title="TextBox" about={"About us"}  />
+     <div className='container py-4'>
+      <Textarea heading="Enter Text Below:" />
+     </div>
     </>
   );
 }
---
+++ src/components/Textarea.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+
+
+
+export default function Textarea(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        }
+
+        const handleDownClick = () => {
+            let newText = text.toLowerCase();
+            setText(newText);
+            }
+    
+
+        
+        const handleOnChange = (event)=>{
+            setText(event.target.value)
+        }
+
+        const [text, setText] = useState('');
+
+    return (  
+        <>
+    <div className='container'>
+        <h1>{props.heading}</h1>
+            <div className='mb-3'>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="textbox" rows="5"></textarea>
+            </div>
+
+         <button type="button" onClick={handleUpClick} className="btn btn-primary">UPPER-CASE</button>
+         
+         <button type="button" onClick={handleDownClick} className="btn btn-primary m-2">lower-case</button>
+    </div>
+
+    <div className="container my-3">
+        <h1>Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+    </div>
+
+    <h2>Preview</h2>
+    <p>{text}</p>
+    </>
+   )
+    
+}
